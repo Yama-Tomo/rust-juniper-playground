80b5a41ccd8aa5cc4ba08c70e2d6370c45e437e4
Yama-Tomo <yam4.tomo@gmail.com>
2022-01-19 16:54:10 +0900

    refactor: Errのパターンと同じようにネストしたResult型を返していることをわかりやすくする記述に変更

--- src/data_sources/post.rs
+++ src/data_sources/post.rs
@@ -57,7 +57,7 @@ impl Datasource {
 
         match build_res {
             Ok(build_res) => match build_res {
-                Ok(model) => model.insert(self.conn.as_ref()).await.map(Ok),
+                Ok(model) => Ok(Ok(model.insert(self.conn.as_ref()).await?)),
                 Err(validation_err) => Ok(Err(validation_err)),
             },
             Err(e) => Err(e),
